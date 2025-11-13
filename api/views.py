4b9e84ed5512d6a6331ef99c0efae21b85e4022f
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-11-13 13:54:36 +0200

    updated API logic to view user by id

--- api/views.py
+++ api/views.py
@@ -20,3 +20,14 @@ def studentsView(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
