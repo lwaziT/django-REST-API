758df49f8fcafaf399d46283b7e1be64035cf7ed
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-11-13 14:33:04 +0200

    Updated API logic to update student details.

--- api/views.py
+++ api/views.py
@@ -13,16 +13,18 @@ def studentsView(request):
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == 'POST':
+        # Send data to the student table
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def studentDetailView(request, pk):
+    #Get student data by id
     try:
         student = Student.objects.get(pk=pk)
     except Student.DoesNotExist:
@@ -31,3 +33,11 @@ def studentDetailView(request, pk):
     if request.method == 'GET':
         serializer = StudentSerializer(student)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
