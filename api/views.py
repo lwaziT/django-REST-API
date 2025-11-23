d351c5d741349e7f53e2ade57bd3473cd0ce07d2
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-11-23 16:35:41 +0200

    Implemented logic for blog feature: implemented logic for primary key based operations.

--- api/views.py
+++ api/views.py
@@ -185,6 +185,16 @@ class BlogsView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    lookup_field = 'pk'
+
 class CommentsView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'pk'
